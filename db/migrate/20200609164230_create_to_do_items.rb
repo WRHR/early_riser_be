7104dc072374215908f3e44dbf549d6dceba7d9a
WRHR <wrhreeves@gmail.com>
2020-06-09 11:03:15 -0600

    Add foreign key to toDoItem

--- db/migrate/20200609164230_create_to_do_items.rb
+++ db/migrate/20200609164230_create_to_do_items.rb
@@ -2,6 +2,7 @@ class CreateToDoItems < ActiveRecord::Migration[6.0]
   def change
     create_table :to_do_items do |t|
       t.string :activity
+      t.references :user, foriegn_key: true
 
       t.timestamps
     end
